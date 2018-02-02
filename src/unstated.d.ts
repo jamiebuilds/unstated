25ef4cb60fc84cd239baf4065bbe371bbc3038fc
Sebastian Sebald <sebastian.sebald@ptw.cloud>
2018-02-02 10:41:02 +0100

    Add typings and tests for TypeScript

--- src/unstated.d.ts
+++ src/unstated.d.ts
@@ -1,1 +1,26 @@
-// Contributions welcome
+import * as React from 'react';
+
+export class Container<State extends object> {
+  state: State;
+  setState(state: Partial<State>): void;
+  subscribe(fn: Function): void;
+  unsubscribe(fn: Function): void;
+}
+
+export interface ContainerType<State extends object> {
+  new (): Container<State>;
+}
+
+interface SubscribeProps {
+  to: ContainerType<any>[];
+  children(...instances: Container<any>[]): React.ReactNode;
+}
+
+export class Subscribe extends React.Component<SubscribeProps> {}
+
+export interface ProviderProps {
+  inject?: Container<any>[];
+  children: React.ReactNode;
+}
+
+export const Provider: React.SFC<ProviderProps>;
