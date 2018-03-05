8df8a8a8241f16b05ff2b8a1dd16106635deee35
Minh Phan <phanminh65@gmail.com>
2018-03-05 12:05:22 +0700

    Supporting either new React.createContext api or new version of create-react-context

--- src/unstated.js
+++ src/unstated.js
@@ -119,23 +119,17 @@ export type ProviderProps = {
 };
 
 export function Provider(props: ProviderProps) {
+  const childMap = new Map();
+
+  if (props.inject) {
+    props.inject.forEach(instance => {
+      childMap.set(instance.constructor, instance);
+    });
+  }
+
   return (
-    <StateContext.Consumer>
-      {parentMap => {
-        let childMap = new Map(parentMap);
-
-        if (props.inject) {
-          props.inject.forEach(instance => {
-            childMap.set(instance.constructor, instance);
-          });
-        }
-
-        return (
-          <StateContext.Provider value={childMap}>
-            {props.children}
-          </StateContext.Provider>
-        );
-      }}
-    </StateContext.Consumer>
+    <StateContext.Provider value={childMap}>
+      {props.children}
+    </StateContext.Provider>
   );
 }
